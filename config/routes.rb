6e653d3de0f5f09cc72b14b8246199d957ab607a
Th0rt <yamaxanado@gmail.com>
2018-05-25 14:57:00 +0900

    [Add] Add messages controller / views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resources :groups, only: [:new, :create, :edit, :update]
+  
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :new, :create]
+  end
 
 end
