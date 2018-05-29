07294cb808482ba28ff71cfb177afa6da2befacc
Th0rt <yamaxanado@gmail.com>
2018-05-29 18:32:38 +0900

    [update] Set temporary root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-
+  root to: 'messages#index'
   devise_for :users
-  
-  resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index, :new, :create]
-  end
+
+  resources :groups, only: [:new, :create, :edit, :update]
+  resources :messages, only: [:index, :new, :create]
 
 end
