365f06f7e0a0950fae1f5768fac72b414031e9a0
Th0rt <yamaxanado@gmail.com>
2018-05-30 19:36:00 +0900

    [update] feature new group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,17 @@
 class GroupsController < ApplicationController
 
   def new
+    @group = Group.new
+    @group.users << current_user
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
   end
 
   def edit
@@ -12,6 +20,18 @@ class GroupsController < ApplicationController
   end
 
   def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group)
+    else
+      render group_edit_path(@group)
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, { :user_ids => [] })
   end
 
 end
