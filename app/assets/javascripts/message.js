53e1f7761ca4ec5c3b8ac0e2732e6caef1012018
Th0rt <yamaxanado@gmail.com>
2018-06-01 19:31:52 +0900

    [update] add html function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,30 @@
 $(function() {
+
+  function append_message(message) {
+
+    var html = `<div class="message">
+                  <div class="message__author-name">
+                    ${ message.user.name }
+                  </div>
+                  <div class="message__time">
+                    ${ message.created_at }
+                  </div>
+                  <div class="message__text">
+                    ${ message.text }
+                  </div>
+                </div>`
+
+    $('.messages-list').append(html);
+    append_image(message)
+  }
+
+  function append_image(message) {
+    if (message.image.url) {
+      var html = `<img src="${message.image.url}" class="message__image">`
+      $('.message:last').append(html);
+    }
+  }
+
   $('.comment-form').on('submit', function(e) {
     e.preventDefault();
 
@@ -15,6 +41,8 @@ $(function() {
     })
     .done(function() {
       console.log("success");
+    .done(function(data) {
+      append_message(data);
     })
     .fail(function() {
       console.log("error");
