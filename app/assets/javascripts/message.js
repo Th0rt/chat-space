0a12ed797fac406f75e667bc1078b2be9d41362e
Th0rt <yamaxanado@gmail.com>
2018-06-01 17:33:57 +0900

    [update] ajax communication

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,28 @@
 $(function() {
   $('.comment-form').on('submit', function(e) {
     e.preventDefault();
-      comment = $('.comment-form__message').val();
-      console.log(comment);
+
+    var fd = new FormData(this);
+    var url = $(this).attr('action')
+
+    $.ajax({
+      url: url,
+      type: 'POST',
+      dataType: 'JSON',
+      data: fd,
+      processData: false,
+      contentType: false
+    })
+    .done(function() {
+      console.log("success");
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      console.log("complete");
+    });
+
 
   })
 })
