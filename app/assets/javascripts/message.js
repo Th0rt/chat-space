9ce1f13572c4fa78cc97e869459b72929882ff80
Th0rt <yamaxanado@gmail.com>
2018-06-02 20:44:42 +0900

    [update] auto update messages veiw with ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -9,6 +9,30 @@ $(function() {
     $('#flash').html(html)
   }
 
+   $(function auto_update(){
+    setInterval(update, 5000);
+  })
+
+  function update(){
+    var last_message_id = $('.message:last').data('id')
+
+    $.ajax({
+      url: location.href,
+      type: 'GET',
+      dataType: 'JSON',
+      data: { last_message_id: last_message_id }
+    })
+    .done(function(data) {
+      if(data.new_message) {
+        $.each(data.new_message,function(i, message) { append_message(message) });
+        scroll_bottom($('.main-body'));
+      }
+    })
+    .fail(function() {
+      append_flash('更新に失敗しました', 'alert');
+    })
+  }
+
   function append_message(message) {
 
     var image_html = (message.image_url)? `<img src="${message.image_url}" class="message__image">` : ""
