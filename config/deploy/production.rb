5003138398445beb5ccdbd75182be37ff5909206
Th0rt <yamaxanado@gmail.com>
2018-06-03 18:40:06 +0900

    [update] add deploy server setting

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
+server "18.179.75.0", user: "ec2-user", roles: %w{app db web}
 
 
 # role-based syntax
